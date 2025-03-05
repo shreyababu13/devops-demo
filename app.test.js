da5ec2d0c69023989dae0acb8639c3c19b7f4245
shreyababu13 <shreya.babu@epita.fr>
2025-03-05 17:49:28 +0100

    bug fixed

--- app.test.js
+++ app.test.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('Hello, DevOps!');
 });
 
-Test("GET / should return 'Hello, DevOps!'", async () => {
+test("GET / should return 'Hello, DevOps!'", async () => {
     const response = await request(app).get('/');
     expect(response.text).toBe('Hello, DevOps!');
 });
